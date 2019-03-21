63bcc5b796e3075be3bf417aaf503a8c5a53e000
Eknut <mean.dp589@gmail.com>
2019-03-21 15:09:00 +0700

    Add cars

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const apiRouter = require("./controllers/vmt");
 const app = express();
-
+const cors = require("cors");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
