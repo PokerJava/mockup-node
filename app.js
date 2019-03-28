047b5f1a1627dcd0097b892bb0e0ff3c9df2cea1
Eknut <mean.dp589@gmail.com>
2019-03-28 16:59:09 +0700

    Change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/vmt", vmtRouter);
 app.use("/ExternalAPI", externalAPIRouter);
-app.use("/shop", shopRouter);
+app.use("/api/shop-cdns/upload", shopRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
