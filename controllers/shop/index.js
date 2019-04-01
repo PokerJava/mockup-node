1a86dd708c4418f05f522f2e92de0b29f3eae012
Eknut <mean.dp589@gmail.com>
2019-04-01 12:58:48 +0700

    Change

--- controllers/shop/index.js
+++ controllers/shop/index.js
@@ -4,22 +4,27 @@ const mongoose = require("mongoose");
 const NotifyTargets = mongoose.model("NotifyTargets");
 const { resultCode } = require("../../utils");
 const randomstring = require("randomstring");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "public/shop");
+  },
+  filename: function(req, file, cb) {
+    let fileSplit = file.originalname.split(",");
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + "." + fileSplit[fileSplit.length - 1]
+    );
+  }
+});
+const upload = multer({ storage: storage });
 
 notifyRouter
-  //   .get("/", async (req, res, next) => {
-  //     try {
-  //       return res.json(buildMessage);
-  //     } catch (err) {
-  //       buildMessage = { ...resultCode(50000), err: err };
-  //       return res.json(buildMessage);
-  //     }
-  //   })
-  .post("/", async (req, res, next) => {
+  .post("/", upload.single("image"), async (req, res, next) => {
     try {
       let buildMessage = {
-        imageUrl: "206.189.41.105:9001/shop/850105679427509.jpg",
-        // imageUrl: "localhost:5555/shop/850105679427509.jpg",
-        name: "850105679427509.jpg",
+        imageUrl: `206.189.41.105:9001/shop/${req.file.filename}`,
+        name: `${req.file.filename}`,
         size: 969465,
         thumbnails: "test1",
         thumbnails43: "test2",
@@ -34,8 +39,7 @@ notifyRouter
   .put("/", async (req, res, next) => {
     try {
       let buildMessage = {
-        imageUrl: "206.189.41.105:5000/shop/850105679427509.jpg",
-        // imageUrl: "localhost:5555/shop/850105679427509.jpg",
+        imageUrl: "206.189.41.105:9001/shop/850105679427509.jpg",
         name: "850105679427509.jpg",
         size: 969465,
         thumbnails: "test1",
