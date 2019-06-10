a56343c484a44c7e1e505cd67aff81adc78c52b8
Eknut <mean.dp589@gmail.com>
2019-06-10 17:04:18 +0700

    add headers

--- controllers/b2c/index.js
+++ controllers/b2c/index.js
@@ -12,7 +12,11 @@ serverRouter.post("/", async (req, res, next) => {
         resultCode: "40401",
         developerMessage: "Data Not Found"
       };
-
+  res.set({
+    "X-Seesion-Id": "123456789",
+    "X-App": "appName=cookieRun;appVersion=1.0.1",
+    "X-Partner-Specific-Private-Id": "partner specific-private"
+  });
   return res.json(resp);
 });
 
