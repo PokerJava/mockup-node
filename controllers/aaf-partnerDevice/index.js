29ced9cc59dab5971a1f86029748f42f15a1e1ba
maxmuffin <imaxer.cs@gmail.com>
2019-06-20 11:39:01 +0700

    Return to 20000

--- controllers/aaf-partnerDevice/index.js
+++ controllers/aaf-partnerDevice/index.js
@@ -10,27 +10,30 @@ serverRouter
     return res.json(resp);
   })
   .get("/:idType/:idValue.json", async (req, res, next) => {
-    // let resp =  {
-    //       resultCode: "20000",
-    //       developerMessage: "Success",
-    //       publicId: ["66123456789"],
-    //       uuid: ["oekfirj94kr040rkeoj303jd0", "oekfirj94kr040rkeo1239"],
-    //       devicelist:[
-    //         "QzhUSjUwMFFGMU1O",
-    //         "QzhUSjUwMFFGMlNX"
-    //       ]
-    //     }
-
      let resp =  {
-          resultCode: "20000",
-          developerMessage: "Success",
-          publicId: ["66123456789"],
-          uuid: ["oekfirj94kr040rkeoj303jd0", "oekfirj94kr040rkeo1239"],
-          devicelist:[
-            "QzhUSjUwMFFGMU1O",
-            "QzhUSjUwMFFGMlNX"
-          ]
-        }
+           resultCode: "20000",
+           developerMessage: "Success",
+           publicId: ["66123456789"],
+           uuid: ["oekfirj94kr040rkeoj303jd0", "oekfirj94kr040rkeo1239"],
+           devicelist:[
+             "QzhUSjUwMFFGMU1O",
+             "QzhUSjUwMFFGMlNX"
+           ]
+         }
+
+    /* let resp =  {  
+      resultCode: "40400",
+      developerMessage: "Data Not Found",
+      errorMessageStack: [  
+         {  
+            node: "SDF",
+            resultCode: "40401",
+            developerMessage: "Not found xxx",
+            moreInfo: "to be define"
+         }
+      ]
+   }
+   */
     return res.status(!req.query["notFound"] ? 200 : 401).json(resp);
   })
   .delete("/:idType/:idValue.json", async (req, res, next) => {
--- controllers/b2c/index.js
+++ controllers/b2c/index.js
@@ -2,28 +2,28 @@ const express = require("express");
 const serverRouter = new express.Router();
 
 serverRouter.post("/", async (req, res, next) => {
-/*  res.set({
+  res.set({
     "x-session-id": "123456789",
     "x-app": "appName=cookieRun;appVersion=1.0.1",
     "x-partner-specific-private-id": "partner specific-private"
   });
-*/  res.set({
+/*  res.set({
     "X-Partner-Specific-Private-Id": "TwrIoVUgj6e2hbSNXNEAXtj09LIGULlrX8iOkD0VCDH=",
     "X-App": "appName=netflixstb; appVersion=1.0.0;",	
     "X-Session-Id": "weww62CCxLcPDLhncIXn"
   
   });
-/*  let resp = {
+*/  let resp = {
         resultCode: "20100",
         developerMessage: "Success",
        privateId: "123fewf34"
       }
-*/      let resp = {
+/*      let resp = {
         resultCode: "50003",
         developerMessage: "connection_timeout",
         privateId: "TwrIoVUgj6e2hbSNXNEAXtj09LIGULlrX8iOkD0VCDH="
       }
-
+*/
   return res.status(201).json(resp);
 });
 
